f894e1d6ddfcd67509b4a4abd3450c466982e0f1
Neil Schelly <neil@neilschelly.com>
2013-03-21 09:26:16 -0400

    * Resetting default recipe to nothing. * Adding copyright/license information to all .rb files. * Cleaning up comments, indentation, etc

--- providers/default.rb
+++ providers/default.rb
@@ -1,3 +1,24 @@
+#
+# Cookbook Name:: encryptfs
+# Attributes:: default
+#
+# Copyright 2013, Neil Schelly
+# Copyright 2013, Dyn, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
 def whyrun_supported?
   true
 end
@@ -12,6 +33,7 @@ action :create do
     end
   end
 end
+
 
 action :delete do
   if @current_resource.exists
@@ -33,6 +55,7 @@ def load_current_resource
   end
 end    
 
+#######################################################
 
 def create_encryptfs
   # Install cryptsetup
@@ -96,6 +119,7 @@ def create_encryptfs
   end
 end
 
+
 def delete_encryptfs
   # Unmount and remove entry from fstab
   mount @new_resource.mountpath do
@@ -129,17 +153,17 @@ def delete_encryptfs
     action :delete
   end  
 
+  # Uninstall cryptsetup packages if configured to do so
   ruby_block "uninstall_cryptsetup" do
     block do
       uninstall_cryptfs if node[:encryptfs][:uninstall_cryptsetup_iflast]
     end
   end
-
 end
 
-def uninstall_cryptfs
-  # Check if there are any active cryptsetup filesystems.
 
+def uninstall_cryptfs
+  # Scan for there non-blank, non-comment lines in crypttab
   ::File.readlines("/etc/crypttab").reverse_each do |line|
     if (!(line =~ /^#/ or line =~ /^\s*$/ ))
       Chef::Log.info("Not removing cryptsetup because crypttab contains encrypted volumes.")
@@ -147,49 +171,64 @@ def uninstall_cryptfs
     end
   end
 
+  # Didn't find any non-blank, non-comment lines in crypttab
   Chef::Log.info("Removing cryptsetup package because crypttab is empty.")
   package "cryptsetup" do
     action :remove
   end
+  
+  # Debian only uses the cryptsetup package, but Ubuntu has both.
   package "cryptsetup-bin" do
     action :remove
   end
-  
 end
 
+
 def encryptfs_exists?(name)
+  # Return code of 0 only when the name exists and is active.
   return system("/sbin/cryptsetup status #{name}")
 end
 
+
 def encryptfs_crypttab_exists?(name)
-    if (! ::File.exists?( "/etc/crypttab" ))
+  # If crypttab doesn't exist, then we know #{name} isn't in it.
+  if (! ::File.exists?( "/etc/crypttab" ))
     return false
   end
+  
+  # Scan through crypttab
   ::File.foreach("/etc/crypttab") do |line|
-    if ( line =~ /^#{name} / )
-      return true
-    end
+    # Return true if we find a line beginning with #{name}
+    return true if ( line =~ /^#{name} / )
   end
+  
+  # Failed to find #{name} in crypttab
   return false
 end
 
+
 def encryptfs_crypttab_add(name, filepath, fstype)
+  # Append newline for the new encryptfs to the /etc/crypttab
   newline = "#{name} \t#{filepath}\t /dev/urandom \ttmp=#{fstype},cipher=aes-cbc-essiv:sha256,noearly\n"
   ::File.open("/etc/crypttab", "a") do |crypttab|
     crypttab.puts(newline)
   end
 end
 
+
 def encryptfs_crypttab_delete(name)
+  # contents will be a list of lines to _keep_ in the file when we rewrite it.
   contents = []
   ::File.readlines("/etc/crypttab").reverse_each do |line|
     if (!(line =~ /^#{name} / ))
       contents << line
     else
+      # Skip copying the deleted encryptfs into the contents array
       Chef::Log.info("#{@new_resource} is removed from crypttab")
     end
   end
   
+  # Write out the contents array as lines in a new /etc/crypttab.
   ::File.open("/etc/crypttab", "w") do |crypttab|
     contents.reverse_each { |line| crypttab.puts line }
   end
